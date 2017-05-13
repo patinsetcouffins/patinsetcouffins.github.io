3e651c1247ff67707ca98a9d83550d31295d0a0a
patinsetcouffins <patinsetcouffins@gmail.com>
2017-05-13 17:32:00 +0200

    Update pageBootstrap.js

--- blogspot/script/pageBootstrap.js
+++ blogspot/script/pageBootstrap.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   
   $.ajax({
-   url:$(this).find('#patinsCouffinsContent').attr('href');,
+   url:$('#patinsCouffinsContent').attr('href');,
    type:'GET',
    success: function(data){
        var htmlData = $(data).html();
