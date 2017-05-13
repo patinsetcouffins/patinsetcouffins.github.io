4719bbcbd4a2a6414ec217160958c4cfdfccda79
patinsetcouffins <patinsetcouffins@gmail.com>
2017-05-13 17:23:39 +0200

    Update pageBootstrap.js

--- blogspot/script/pageBootstrap.js
+++ blogspot/script/pageBootstrap.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-  $("#patinsCouffinsContent").html( "<h1>The DOM is now loaded and can be manipulated.</h1>" );
+  
+  $.ajax({
+   url:$(this).find('#patinsCouffinsContent').attr('href');,
+   type:'GET',
+   success: function(data){
+       var htmlData = $(data).html();
+       $('#patinsCouffinsContent').html(htmlData);
+   }
+});
+  
+  //$("#patinsCouffinsContent").html( "<h1>The DOM is now loaded and can be manipulated.</h1>" );
 });
